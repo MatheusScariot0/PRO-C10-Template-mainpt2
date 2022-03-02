ad05e51ac4f6592d5d0585c95e170d6150fc0285
MatheusScariot0 <100584856+MatheusScariot0@users.noreply.github.com>
2022-03-02 11:42:23 -0300

    second commit

--- sketch.js
+++ sketch.js
@@ -5,10 +5,9 @@ function preload(){
   // Descomente o código para adicionar animação ao navio
 
   shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  
   
   seaImg = loadImage("sea.png");
 }
@@ -23,7 +22,7 @@ function setup(){
   sea.velocityX = -5;
   sea.scale=0.3;
 
-  
+
   
   ship = createSprite(130,200,30,30);
   ship.addAnimation("movingShip",shipImg1);
@@ -37,10 +36,9 @@ function draw() {
 
   // Descomente o código para redefinir o fundo
   if(sea.x < 0){
-    //sea.x = 0;
-    //sea.x = sea.width;
-    //sea.x = sea.width/8;
-    //sea.y = height;
+  
+   sea.x = sea.width/8;
+    
   }
 
  
